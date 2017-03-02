0481d9ad437e9e5f62d14e28029fe2fdf1e1e6c0
lykovrs <lykovrs@gmail.com>
2017-03-02 12:50:41 +0300

    add doc

--- angular2calendar/src/app/calendar/calendar.component.ts
+++ angular2calendar/src/app/calendar/calendar.component.ts
@@ -1,20 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { StoreService } from '../store.service';
-
+/**
+ * Render calendar
+ * 
+ * @export
+ * @class CalendarComponent
+ * @implements {OnInit}
+ */
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css']
 })
-export class CalendarComponent implements OnInit {
-  public days = [];
-  public hours = [];
+export class CalendarComponent {
+  public days: string[] = [];
+  public hours: number[] = [];
   public isLoaded: boolean = false;
 
   private cells = 15;
   private colls = 24;
 
-
+  /**
+   * add colls and rows in table
+   * 
+   * 
+   * @memberOf CalendarComponent
+   */
   renderTable(): void {
 
     this.days = [];
@@ -32,23 +43,31 @@ export class CalendarComponent implements OnInit {
 
   }
 
-
+  /**
+   * Control loading and render table
+   * 
+   * 
+   * @memberOf CalendarComponent
+   */
   loadData(): void {
     this.isLoaded = true;
     this.renderTable()
   }
 
+  // TODO: add the ability to change a lot of data asynchronously
 
   // searchAll() {
   //   this.store.searchAllCells();
   // }
 
 
+  /**
+   * Click to head table
+   * 
+   * 
+   * @memberOf CalendarComponent
+   */
   dayHeaderClicked(): void {
     alert('dayHeaderClicked()');
   }
-
-  ngOnInit() {
-  }
-
 }
